e9de9fe92653807cd74bf08cfb56147988ee1d11
phildong <35715936+phildong@users.noreply.github.com>
2025-06-23 16:00:30 -0400

    update testing

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,6 +10,9 @@ from fed3.utilities import FED3DATA, export_summary
 pn.extension()
 
 IN_DPATH = "./data/20250606/"
+OUT_PATH = "./output/"
+
+os.makedirs(OUT_PATH, exist_ok=True)
 
 # %% select data
 dpaths = []
@@ -20,5 +23,11 @@ fed_data = FED3DATA()
 fed_data.select_data(dpaths)
 fed_data.assign_metadata(rel_start=True)
 
-# %% load data
+# %% load data and process
 fed_data.load_data()
+fig_daynight = fplot.daynight_bar(
+    fed_data.data_combined, agg_grp="sum", tcol="elps_time"
+)
+fig_msize = fplot.mealsize_hist(fed_data.data_combined)
+summary = export_summary(fed_data.data_combined)
+summary.to_csv(os.path.join(OUT_PATH, "summary.csv"))
