22cbea84be1137b6bfe18676981594a1ffd21d9c
phildong <35715936+phildong@users.noreply.github.com>
2025-06-23 16:37:45 -0400

    updated test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,6 +10,88 @@ from fed3.utilities import FED3DATA, export_summary
 pn.extension()
 
 IN_DPATH = "./data/20250606/"
+PARAM_META_DICT = {
+    "./data/20250606/mCherry/FED025_060424_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal0",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED028_051524_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal1",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED021_060424_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal2",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED038_060424_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal3",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED008_060124_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal4",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED042_060124_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal5",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED039_060124_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal6",
+        "start_time": "start",
+    },
+    "./data/20250606/mCherry/FED003_060124_00.CSV": {
+        "group": "mCherry",
+        "animal": "animal7",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED024_060124_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal8",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED023_051524_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal9",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED022_060124_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal10",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED018_080124_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal11",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED016_051524_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal12",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED043_080124_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal13",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED030_060424_00.CSV": {
+        "group": "hM4d",
+        "animal": "animal14",
+        "start_time": "start",
+    },
+    "./data/20250606/hM4d/FED008_072624_01.CSV": {
+        "group": "hM4d",
+        "animal": "animal15",
+        "start_time": "start",
+    },
+}
 OUT_PATH = "./output/"
 
 os.makedirs(OUT_PATH, exist_ok=True)
@@ -25,7 +107,7 @@ fed_data.assign_metadata(rel_start=True)
 
 # %% load data and process
 fed_data.load_data()
-fig_daynight = fplot.daynight_bar(
+fig_daynight = fplot.daynight_plot(
     fed_data.data_combined, agg_grp="sum", tcol="elps_time"
 )
 fig_msize = fplot.mealsize_hist(fed_data.data_combined)
